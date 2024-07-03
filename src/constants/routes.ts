905094a974937fd73e784562f1daa73977437f58
bada308 <203190@jnu.ac.kr>
2024-07-03 15:53:36 +0900

    feat: 라우터 url 상수화

---
+++ src/constants/routes.ts
@@ -0,0 +1,14 @@
+const routes = {
+  // Domain
+  main: '/',
+  signup: '/signup',
+  login: '/login',
+  challenge: '/challenge',
+  family: '/family',
+  mypage: '/mypage',
+  recap: '/recap',
+  // Action
+  create: '/create',
+} as const;
+
+export default routes;
