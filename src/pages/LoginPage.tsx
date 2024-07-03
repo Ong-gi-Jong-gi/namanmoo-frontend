132871456dbd398bed58dc654ff841834cac29ae
bada308 <203190@jnu.ac.kr>
2024-07-04 00:00:43 +0900

    feat: 회원가입 페이지에 폼 적용

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,9 @@
 import LoginForm from '../components/login/LoginForm';
 
 const LoginPage = () => {
+  // FIXME: width 제거 및 레이아웃 적용
   return (
-    <div>
+    <div className="w-[288px]">
       <LoginForm />
     </div>
   );
