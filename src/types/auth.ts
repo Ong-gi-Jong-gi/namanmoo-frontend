5ebc1c032246e21cf151511f4f343b46285eaa09
bada308 <203190@jnu.ac.kr>
2024-07-03 23:58:22 +0900

    feat: 회원가입 폼 검증 함수 구현

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -2,3 +2,11 @@ export interface LoginValues {
   userId: string;
   password: string;
 }
+
+export interface SignupValues {
+  name: string;
+  nickname: string;
+  userId: string;
+  password: string;
+  passwordConfirm: string;
+}
