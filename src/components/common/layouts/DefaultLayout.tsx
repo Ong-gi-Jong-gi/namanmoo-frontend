09c61c8099534bb8e351cbae50c2e9c5390b0a4d
bada308 <203190@jnu.ac.kr>
2024-07-04 23:46:53 +0900

    feat: 기본 레이아웃 구현

---
+++ src/components/common/layouts/DefaultLayout.tsx
@@ -0,0 +1,21 @@
+import clsx from 'clsx';
+import { Outlet, useLocation } from 'react-router-dom';
+import routes from '../../../constants/routes';
+
+const DefaultLayout = () => {
+  const { pathname } = useLocation();
+  const layoutClass = clsx(
+    'm-auto h-full w-full max-w-[450px] overflow-y-scroll bg-background p-12 scrollbar-hide',
+    {
+      'bg-main': pathname === routes.main,
+      'bg-background': pathname !== routes.main,
+    },
+  );
+  return (
+    <div className={layoutClass}>
+      <Outlet />
+    </div>
+  );
+};
+
+export default DefaultLayout;
