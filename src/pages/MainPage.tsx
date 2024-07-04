3bfeb6a9d578e8ec0dcd2bc6b06eea9277b1833c
bada308 <203190@jnu.ac.kr>
2024-07-04 23:47:18 +0900

    feat: 페이지 레이아웃 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -5,13 +5,12 @@ import Lucky from '../components/main/Lucky';
 const MainPage = () => {
   // FIXME: width 값 제거 및 레이아웃 적용
   return (
-    <div className="w-[288px]">
+    <div className="h-full w-full">
       <FamilyList />
-      <Lucky level={1} />
-      <Lucky level={2} />
-      <Lucky level={3} />
-      <Lucky level={4} />
-      <ChallengeButton />
+      <div className="grid h-full w-full grid-rows-[1fr_140px] items-end pt-32">
+        <Lucky level={1} />
+        <ChallengeButton />
+      </div>
     </div>
   );
 };
