eac40c83c6a95eece7fce2b62196a4e6ce6fd003
Parksangwoo <psu8430@gmail.com>
2024-07-05 00:37:38 +0900

    feat: Add Modal Provider in App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { RouterProvider } from 'react-router-dom';
+import ModalProvider from './components/common/Modal';
 import router from './routes';
 
 function App() {
   return (
     <>
       <RouterProvider router={router} />
+      <ModalProvider />
     </>
   );
 }
