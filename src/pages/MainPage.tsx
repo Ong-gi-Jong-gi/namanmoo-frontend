5f47dbf2ec195f929f6076ae3df82e325ceda55f
bada308 <203190@jnu.ac.kr>
2024-07-04 17:55:47 +0900

    feat: 챌린지 버튼 컴포넌트 구현 및 메인 페이지에 적용

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,17 @@
+import ChallengeButton from '../components/main/ChallengeButton';
 import FamilyList from '../components/main/FamilyList';
+import Lucky from '../components/main/Lucky';
 
 const MainPage = () => {
   // FIXME: width 값 제거 및 레이아웃 적용
   return (
     <div className="w-[288px]">
       <FamilyList />
+      <Lucky level={1} />
+      <Lucky level={2} />
+      <Lucky level={3} />
+      <Lucky level={4} />
+      <ChallengeButton />
     </div>
   );
 };
