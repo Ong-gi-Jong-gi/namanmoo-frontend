59913b40c55b553eb5ff582ef110598bd405116a
bada308 <203190@jnu.ac.kr>
2024-07-04 16:43:18 +0900

    feat: 메인 페이지에 FamilyList 컴포넌트 적용

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,12 @@
+import FamilyList from '../components/main/FamilyList';
+
 const MainPage = () => {
-  return <div>메인 페이지</div>;
+  // FIXME: width 값 제거 및 레이아웃 적용
+  return (
+    <div className="w-[288px]">
+      <FamilyList />
+    </div>
+  );
 };
 
 export default MainPage;
