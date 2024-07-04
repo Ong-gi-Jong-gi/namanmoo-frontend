6192ec606f4ba40c5920d718e8d6eef6d63c1253
bada308 <203190@jnu.ac.kr>
2024-07-04 20:49:42 +0900

    feat: 메인 페이지 컴포넌트 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -5,13 +5,10 @@ import Lucky from '../components/main/Lucky';
 const MainPage = () => {
   // FIXME: width 값 제거 및 레이아웃 적용
   return (
-    <div className="w-[288px]">
+    <div className="bg-main h-[844px] w-[390px] bg-contain bg-center bg-no-repeat">
       <FamilyList />
-      <Lucky level={1} />
       <Lucky level={2} />
-      <Lucky level={3} />
-      <Lucky level={4} />
-      <ChallengeButton />
+      <ChallengeButton type="ongoing" text="챌린지 시작" day={12} />
     </div>
   );
 };
