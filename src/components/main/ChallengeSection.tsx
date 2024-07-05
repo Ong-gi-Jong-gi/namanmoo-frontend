846f557751e03dd214259bb2aabd05dd29776e6e
bada308 <203190@jnu.ac.kr>
2024-07-06 00:33:51 +0900

    feat: ChallengeSection 컴포넌트 구조 변경

--- src/components/main/ChallengeSection.tsx
+++ src/components/main/ChallengeSection.tsx
@@ -11,37 +11,43 @@ const ChallengeSection = ({ currentFamilySize }: ChallengeSectionProps) => {
   const { data: challengeInfo, isLoading } = useGetTodayChallenge({
     enabled: currentFamilySize === MAX_FAMILY_MEMBER,
   });
-
-  if (currentFamilySize < MAX_FAMILY_MEMBER)
-    return (
-      <div className="grid h-full w-full grid-rows-[1fr_36%] items-end pt-32">
-        <Lucky level={1} />
-        <ChallengeButton
-          type="disabled"
-          text="챌린지 시작"
-          currentSize={currentFamilySize}
-          familySize={MAX_FAMILY_MEMBER}
-        />
-      </div>
-    );
   if (isLoading) return <div>챌린지 정보 Loading...</div>;
-  if (challengeInfo === null)
+
+  const renderContent = () => {
+    if (currentFamilySize < MAX_FAMILY_MEMBER)
+      return (
+        <>
+          <Lucky level={1} />
+          <ChallengeButton
+            type="disabled"
+            text="챌린지 시작"
+            currentSize={currentFamilySize}
+            familySize={MAX_FAMILY_MEMBER}
+          />
+        </>
+      );
+    if (!challengeInfo)
+      return (
+        <>
+          <Lucky level={1} />
+          <ChallengeButton type="active" text="챌린지 시작" />
+        </>
+      );
     return (
-      <div className="grid h-full w-full grid-rows-[1fr_36%] items-end pt-32">
-        <Lucky level={1} />
-        <ChallengeButton type="active" text="챌린지 시작" />
-      </div>
+      <>
+        <Lucky level={3} />
+        <ChallengeButton
+          type="ongoing"
+          text={challengeInfo.challengeTitle}
+          day={challengeInfo.challengeNumber}
+        />
+      </>
     );
-  if (!challengeInfo) return <div>챌린지 정보가 없습니다.</div>;
+  };
 
   return (
     <div className="grid h-full w-full grid-rows-[1fr_36%] items-end pt-32">
-      <Lucky level={3} />
-      <ChallengeButton
-        type="ongoing"
-        text={challengeInfo.challengeTitle}
-        day={challengeInfo.challengeNumber}
-      />
+      {renderContent()}
     </div>
   );
 };
