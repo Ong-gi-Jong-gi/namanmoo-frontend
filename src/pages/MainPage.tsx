f807c03efe141cdd7f0461b9cd9675847828817b
bada308 <203190@jnu.ac.kr>
2024-07-06 00:22:24 +0900

    feat: 메인 페이지에서 챌린지 관련 컴포넌트 분리

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,8 @@
 import { useEffect } from 'react';
 import { useGetMyFamilyInfo } from '../apis/family/getMyFamilyInfo';
-import ChallengeButton from '../components/main/ChallengeButton';
+import ChallengeSection from '../components/main/ChallengeSection';
 import FamilyList from '../components/main/FamilyList';
 import InviteModal from '../components/main/InviteModal';
-import Lucky from '../components/main/Lucky';
 import Navbar from '../components/main/Navbar';
 import useModalStore from '../store/modalStore';
 
@@ -19,18 +18,14 @@ const MainPage = () => {
     }
   }, [familyList, openModal]);
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <div>가족 정보 Loading...</div>;
   if (!familyList) return <div>가족 정보가 없습니다.</div>;
 
-  // FIXME: width 값 제거 및 레이아웃 적용
   return (
     <div className="h-full w-full">
       <FamilyList familyList={familyList} />
       <Navbar />
-      <div className="grid h-full w-full grid-rows-[1fr_36%] items-end pt-32">
-        <Lucky level={4} />
-        <ChallengeButton type="active" text="챌린지 시작" />
-      </div>
+      <ChallengeSection currentFamilySize={familyList.length} />
     </div>
   );
 };
