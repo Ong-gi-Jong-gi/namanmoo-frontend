c7f97c4eff1ff77114bc509fa6cedc5695dea8ac
bada308 <203190@jnu.ac.kr>
2024-07-05 21:05:20 +0900

    feat: QueryClientProvider 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-    </>
+    </QueryClientProvider>
   );
 }
 
