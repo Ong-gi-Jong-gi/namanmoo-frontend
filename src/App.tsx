9fa1715f9165b6bbd6608711644357acecb4b2b9
bada308 <203190@jnu.ac.kr>
2024-07-06 00:22:54 +0900

    feat: query retry false로 설정

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import ModalProvider from './components/common/Modal';
 import router from './routes';
 
 function App() {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: { queries: { retry: false } },
+  });
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
