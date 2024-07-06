659691cbd9a0d41bc6d0b46461ec3df114225542
bada308 <203190@jnu.ac.kr>
2024-07-06 22:58:03 +0900

    feat: 일반 챌린지 조회 query 개선

--- src/apis/challenge/getNormalChallenge.ts
+++ src/apis/challenge/getNormalChallenge.ts
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import api from '..';
 import API from '../../constants/API';
 import { ChallengeAnswer } from '../../types/challenge';
+import { separateMyAnswer } from '../../utils/separator';
 import { ChallengeAnswerDto, ChallengeDetailDto } from '../dtos/challengeDtos';
 
 const getNormalChallenge = async (challengeId: string) => {
@@ -18,12 +19,25 @@ const getNormalChallenge = async (challengeId: string) => {
 };
 
 export const useGetNormalChallenge = ({
-  challengeId,
+  challengeId: challengeId,
 }: {
-  challengeId: string;
+  challengeId: string | undefined;
 }) => {
-  return useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: [API.CHALLENGE.NORMAL, challengeId],
-    queryFn: () => getNormalChallenge(challengeId),
+    queryFn: () => getNormalChallenge(challengeId || ''),
   });
+
+  const hasData = !!data;
+  const { myAnswer, members } = hasData
+    ? separateMyAnswer(data.answerList)
+    : { myAnswer: null, members: [] };
+
+  return {
+    isLoading,
+    hasData,
+    myAnswer,
+    answerList: members,
+    challengeInfo: data?.challengeInfo,
+  };
 };
