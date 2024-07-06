e2e103c40ab42b17b9c0b88df9a7bf04018c0831
bada308 <203190@jnu.ac.kr>
2024-07-06 17:43:58 +0900

    feat: 일반 챌린지 페이지에 조회 api 연결

--- src/components/qa-challenge/AnswerField.tsx
+++ src/components/qa-challenge/AnswerField.tsx
@@ -1,11 +1,30 @@
+import clsx from 'clsx';
+import { UserRole } from '../../types/family';
 import Profile from '../common/Profile';
 
-const AnswerField = () => {
+interface AnswerFieldProps {
+  nickname: string;
+  role: UserRole;
+  answer: string | null;
+  userImg?: string;
+}
+
+const AnswerField = ({ nickname, role, answer, userImg }: AnswerFieldProps) => {
+  const answerClass = clsx('font-ryurue text-ryurue-base', {
+    'text-gray-40': !answer,
+  });
   return (
     <div className="flex flex-col gap-5">
-      <Profile layout="horizontal" isText userName="미미" userRole="딸" />
-      <span className="font-ryurue text-ryurue-base">
-        냉장고에 넣어둔 아이스크림 다 먹어버린 거 미안해.
+      <Profile
+        type={userImg ? 'image' : 'default'}
+        layout="horizontal"
+        isText
+        userName={nickname}
+        userRole={role}
+        src={userImg}
+      />
+      <span className={answerClass}>
+        {answer || '아직 답변하지 않았습니다.'}
       </span>
     </div>
   );
--- src/components/qa-challenge/NormalChallengeContainer.tsx
+++ src/components/qa-challenge/NormalChallengeContainer.tsx
@@ -1,7 +1,18 @@
+import { useParams } from 'react-router-dom';
+import { useGetNormalChallenge } from '../../apis/challenge/getNormalChallenge';
+import { separateMyAnswer } from '../../utils/separator';
 import AnswerField from './AnswerField';
 import ChallengeHeader from './ChallengeHeader';
 
 const NormalChallengeContainer = () => {
+  const { challengeId } = useParams();
+  const { data, isLoading } = useGetNormalChallenge({
+    challengeId: challengeId || '',
+  });
+  if (isLoading) return <div>Loading...</div>;
+  if (!data) return <div>답변 조회 실패</div>;
+  const { myAnswer, members: answerList } = separateMyAnswer(data.answerList);
+  if (!myAnswer) return <div>내 답변이 없습니다.</div>;
   return (
     <div className="flex h-full w-full flex-col gap-16">
       <ChallengeHeader
@@ -10,10 +21,16 @@ const NormalChallengeContainer = () => {
         challengeTitle="가족에게 미안했지만, 사과하지 못한 순간이 있나요?"
       />
       <div className="flex flex-col gap-12">
-        <AnswerField />
-        <AnswerField />
-        <AnswerField />
-        <AnswerField />
+        <AnswerField
+          nickname="나"
+          role={myAnswer.role}
+          key={myAnswer.userId}
+          userImg={myAnswer.userImg}
+          answer={myAnswer.answer}
+        />
+        {answerList.map((answer) => (
+          <AnswerField key={answer.userId} {...answer} />
+        ))}
       </div>
     </div>
   );
