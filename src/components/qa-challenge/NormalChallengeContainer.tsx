c61c2bbafcf5e9f24843bb0061282684ce48f3eb
bada308 <203190@jnu.ac.kr>
2024-07-06 20:55:46 +0900

    feat: 나의 답변 필드 구현 - 수정 / 조회 상태 존재

--- src/components/qa-challenge/NormalChallengeContainer.tsx
+++ src/components/qa-challenge/NormalChallengeContainer.tsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { useGetNormalChallenge } from '../../apis/challenge/getNormalChallenge';
 import { separateMyAnswer } from '../../utils/separator';
+import AnswerEditor from './AnswerEditor';
 import AnswerField from './AnswerField';
 import ChallengeHeader from './ChallengeHeader';
 
@@ -12,7 +13,7 @@ const NormalChallengeContainer = () => {
   if (isLoading) return <div>Loading...</div>;
   if (!data) return <div>답변 조회 실패</div>;
   const { myAnswer, members: answerList } = separateMyAnswer(data.answerList);
-  if (!myAnswer?.answer) return <div>내 답변이 없습니다.</div>;
+  if (!myAnswer) return <div>내 정보가 존재하지 않습니다.</div>;
   return (
     <div className="flex h-full w-full flex-col gap-16">
       <ChallengeHeader
@@ -21,15 +22,18 @@ const NormalChallengeContainer = () => {
         challengeTitle={data.challengeInfo.challengeTitle}
       />
       <div className="flex flex-col gap-12">
-        <AnswerField
-          nickname="나"
+        <AnswerEditor
           role={myAnswer.role}
           key={myAnswer.userId}
           userImg={myAnswer.userImg}
           answer={myAnswer.answer}
         />
         {answerList.map((answer) => (
-          <AnswerField key={answer.userId} canView={false} {...answer} />
+          <AnswerField
+            key={answer.userId}
+            canView={myAnswer.answer !== null}
+            {...answer}
+          />
         ))}
       </div>
     </div>
