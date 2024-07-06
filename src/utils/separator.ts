5384c363ec121b0d2e5800d20c43929cfa9dfa00
bada308 <203190@jnu.ac.kr>
2024-07-06 17:42:35 +0900

    feat: answerList에서 내 답변과 가족의 답변을 분리하는 코드 작성

--- src/utils/separator.ts
+++ src/utils/separator.ts
@@ -1,13 +1,14 @@
+import { ChallengeAnswerDto } from '../apis/dtos/challengeDtos';
 import { UserInfo } from '../types/user';
 
+// FIXME: 현재 로그인한 유저 id를 어떻게 가져올지 결정해야 함
+const userId = 'wer122a';
 interface MyFamilyInfo {
   myInfo: UserInfo | null;
   members: UserInfo[];
 }
 
 export const separateMyInfo = (userList: UserInfo[]) => {
-  // FIXME: 현재 로그인한 유저 id를 어떻게 가져올지 결정해야 함
-  const userId = 'wer122a';
   return userList.reduce(
     (acc, cur) => {
       if (cur.userId === userId) {
@@ -16,5 +17,25 @@ export const separateMyInfo = (userList: UserInfo[]) => {
       return { ...acc, members: [...acc.members, cur] };
     },
     { myInfo: null, members: [] } as MyFamilyInfo,
+  );
+};
+
+interface FamilyAnswer {
+  myAnswer: ChallengeAnswerDto | null;
+  members: ChallengeAnswerDto[];
+}
+
+export const separateMyAnswer = (answerList: ChallengeAnswerDto[]) => {
+  return answerList.reduce(
+    (acc, cur) => {
+      if (cur.userId === userId) {
+        return { ...acc, myAnswer: cur };
+      }
+      return { ...acc, members: [...acc.members, cur] };
+    },
+    {
+      myAnswer: null,
+      members: [],
+    } as FamilyAnswer,
   );
 };
