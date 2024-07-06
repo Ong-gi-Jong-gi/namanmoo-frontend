e423cd5e86c85a07ebf468ec4ffd8dfc081c40e8
bada308 <203190@jnu.ac.kr>
2024-07-06 17:41:19 +0900

    feat: Header 컴포넌트 UI 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,10 +1,10 @@
 import { FiX } from 'react-icons/fi';
 const Header = () => {
   return (
-    <div className="sticky top-0 mb-6 flex h-14 w-full max-w-[450px] items-center justify-between bg-background">
+    <div className="sticky top-0 flex w-full max-w-[450px] items-center justify-between bg-background py-6">
       <div></div>
       <div>
-        <FiX size={36} className="text-gray-50" />
+        <FiX size={32} className="text-gray-50" />
       </div>
     </div>
   );
