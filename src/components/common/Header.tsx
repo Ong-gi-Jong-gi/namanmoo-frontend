cdf9ee54e739b0f9faf5f116ef2ac46ece2dad7b
bada308 <203190@jnu.ac.kr>
2024-07-06 21:25:19 +0900

    feat: Header X 마크 클릭 시 메인 화면으로 이동

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,10 +1,17 @@
 import { FiX } from 'react-icons/fi';
+import { useNavigate } from 'react-router-dom';
+import routes from '../../constants/routes';
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <div className="sticky top-0 flex w-full max-w-[450px] items-center justify-between bg-background py-6">
       <div></div>
       <div>
-        <FiX size={32} className="text-gray-50" />
+        <FiX
+          size={32}
+          className="text-gray-50"
+          onClick={() => navigate(routes.main)}
+        />
       </div>
     </div>
   );
