3e48cc739b458f0b91de8acaa24ef8b54714014d
bada308 <203190@jnu.ac.kr>
2024-07-06 17:42:06 +0900

    feat: 1문1답 챌린지 관련 타입 및 DTO 선언

--- src/types/challenge.ts
+++ src/types/challenge.ts
@@ -1,3 +1,5 @@
+import { UserInfo } from './user';
+
 type ChallengeType = 'NormalC' | 'GroupC' | 'FaceTimeC' | 'PhotoC' | 'VoiceC';
 
 export interface ChallengeInfo {
@@ -5,4 +7,17 @@ export interface ChallengeInfo {
   challengeNumber: string;
   challengeTitle: string;
   challengeType: ChallengeType;
+}
+
+export interface ChallengeDetail
+  extends Omit<ChallengeInfo, 'challengeId' | 'challengeType'> {
+  challengeDate: string;
+  isCompleted: boolean;
+}
+
+export interface ChallengeGroupDetail
+  extends Omit<ChallengeDetail, 'challengeTitle'> {}
+
+export interface ChallengeAnswer extends Omit<UserInfo, 'name'> {
+  answer: string;
 }
