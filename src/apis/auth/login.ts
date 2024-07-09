830e6778439d64794710e7b04c77a3a732fe2b03
Parksangwoo <psu8430@gmail.com>
2024-07-09 21:02:09 +0900

    feat: Add login api logic

--- src/apis/auth/login.ts
+++ src/apis/auth/login.ts
@@ -1,18 +1,22 @@
 import { useMutation } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 import { useNavigate } from 'react-router-dom';
-import api from '..';
+import { api } from '..';
 import API from '../../constants/API';
 import routes from '../../constants/routes';
-import { LoginValues } from '../../types/auth';
-import { setCookie } from '../../utils/cookie'; // 쿠키 설정 함수 import
+import { responseRoot } from '../../types/api';
+import { LoginValues, UserLoginType } from '../../types/auth';
+import { setCookie } from '../../utils/cookie';
 
 const postLogin = async (loginId: string, password: string) => {
   try {
-    const { data, headers } = await api.post(API.AUTH.LOGIN, {
-      loginId,
-      password,
-    });
+    const { data, headers } = await api.post<responseRoot<UserLoginType>>(
+      API.AUTH.LOGIN,
+      {
+        loginId,
+        password,
+      },
+    );
 
     if (data.status === '401') {
       throw new Error('아이디 또는 비밀번호가 일치하지 않습니다.');
@@ -37,7 +41,7 @@ export const usePostLogin = () => {
     mutationKey: [API.AUTH.LOGIN],
     mutationFn: ({ userId, password }: LoginValues) =>
       postLogin(userId, password),
-    onSuccess: ({ authorization }) => {
+    onSuccess: ({ data, authorization }) => {
       const expireDate = new Date();
       expireDate.setMinutes(expireDate.getMinutes() + 5);
 
@@ -46,7 +50,9 @@ export const usePostLogin = () => {
         secure: true,
         expires: expireDate,
       });
-      navigate(routes.main);
+
+      if (data.familyId) navigate(routes.family.entry);
+      else navigate(routes.main);
     },
     onError: (error: Error) => {
       alert(error.message);
