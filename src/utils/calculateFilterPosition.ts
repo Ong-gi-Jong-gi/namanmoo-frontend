36f8f38201955463d95095aa0212ecfaa62926a2
bada308 <203190@jnu.ac.kr>
2024-07-09 11:22:25 +0900

    feat: 선글라스 위치 계산 함수 수정

--- src/utils/calculateFilterPosition.ts
+++ src/utils/calculateFilterPosition.ts
@@ -7,8 +7,8 @@ const facePoint = {
 };
 
 export const calculateSunglassesPosition = (keypoints: Keypoint[]) => {
-  const xPadding = 30;
-  const yPadding = 10;
+  const xPadding = 24;
+  const yPadding = 8;
 
   const x = keypoints[facePoint.leftEyeTop].x - xPadding;
   const y = keypoints[facePoint.leftEyeTop].y - yPadding;
@@ -21,10 +21,17 @@ export const calculateSunglassesPosition = (keypoints: Keypoint[]) => {
     keypoints[facePoint.leftEyeTop].y +
     yPadding * 2;
 
+  const deltaX =
+    keypoints[facePoint.rightEyeTop].x - keypoints[facePoint.leftEyeTop].x;
+  const deltaY =
+    keypoints[facePoint.rightEyeTop].y - keypoints[facePoint.leftEyeTop].y;
+  const angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI); // Convert from radians to degrees
+
   return {
     x,
     y,
     width,
     height,
+    angle,
   };
 };
