bf507e6dc713330dc504b1ab0287b98f7bb83653
bada308 <203190@jnu.ac.kr>
2024-07-09 15:12:22 +0900

    feat: 필터 렌더링 최적화

--- src/components/face-challenge/FaceFilter.tsx
+++ src/components/face-challenge/FaceFilter.tsx
@@ -1,17 +1,50 @@
 import { useTrackRefContext } from '@livekit/components-react';
-import { type FaceLandmarksDetector } from '@tensorflow-models/face-landmarks-detection';
+import {
+  Face,
+  type FaceLandmarksDetector,
+} from '@tensorflow-models/face-landmarks-detection';
 import { useCallback, useEffect, useRef, useState } from 'react';
+import useFaceLandmarkerStore from '../../store/faceLandmarkerStore';
 import { calculateSunglassesPosition } from '../../utils/calculateFilterPosition';
-import { loadFaceLandmarker } from '../../utils/loadModel';
 
 function FaceFilter() {
+  const { faceLandmarker } = useFaceLandmarkerStore();
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const initialLoadedRef = useRef<boolean>(false);
-  const [status, setStatus] = useState<
-    'Initializing...' | 'Load Model...' | 'Model Loaded'
-  >('Initializing...');
   const trackRef = useTrackRefContext();
   const trackCanvasRef = useRef<HTMLCanvasElement>(null);
+  const [image, setImage] = useState<HTMLImageElement | null>(null);
+  const [canvasContext, setCanvasContext] =
+    useState<CanvasRenderingContext2D | null>(null);
+
+  const loadImageAndSetupCanvas = useCallback(() => {
+    const canvas = canvasRef.current;
+    const context = canvas?.getContext('2d');
+    if (!context || initialLoadedRef.current) return;
+
+    initialLoadedRef.current = true;
+
+    const img = new Image();
+    img.src = 'src/assets/filter/sunglasses.png';
+    img.onload = () => setImage(img);
+
+    setCanvasContext(context);
+  }, []);
+
+  const drawSunglasses = useCallback(
+    (face: Face, ctx: CanvasRenderingContext2D, image: HTMLImageElement) => {
+      const { x, y, width, height, angle } = calculateSunglassesPosition(
+        face.keypoints,
+      );
+
+      ctx.translate(x + width / 2, y + height / 2);
+      ctx.rotate((angle * Math.PI) / 180);
+      ctx.drawImage(image, -width / 2, -height / 2, width, height);
+      ctx.rotate((-angle * Math.PI) / 180);
+      ctx.translate(-(x + width / 2), -(y + height / 2));
+    },
+    [],
+  );
 
   const estimateFacesLoop = useCallback(
     (
@@ -40,47 +73,29 @@ function FaceFilter() {
 
       if (!trackImageData) return;
 
-      model.estimateFaces(trackImageData).then((face) => {
-        canvasRef.current!.width = actualWidth;
-        canvasRef.current!.height = actualHeight;
-
-        ctx.clearRect(0, 0, actualWidth, actualHeight);
-        if (face[0]) {
-          const { x, y, width, height, angle } = calculateSunglassesPosition(
-            face[0].keypoints,
-          );
-
-          ctx.translate(x + width / 2, y + height / 2);
-          ctx.rotate((angle * Math.PI) / 180);
-          ctx.drawImage(image, -width / 2, -height / 2, width, height);
-          ctx.rotate((-angle * Math.PI) / 180);
-          ctx.translate(-(x + width / 2), -(y + height / 2));
+      model.estimateFaces(trackImageData).then((faces) => {
+        if (faces.length > 0) {
+          canvasRef.current!.width = actualWidth;
+          canvasRef.current!.height = actualHeight;
+          ctx.clearRect(0, 0, actualWidth, actualHeight);
+          drawSunglasses(faces[0], ctx, image);
         }
         requestAnimationFrame(() => estimateFacesLoop(model, image, ctx));
       });
     },
-    [trackRef],
+    [trackRef, drawSunglasses],
   );
 
   useEffect(() => {
-    const canvasContext = canvasRef.current?.getContext('2d');
+    loadImageAndSetupCanvas();
+  }, [loadImageAndSetupCanvas]);
 
-    if (!canvasContext || initialLoadedRef.current) return;
-
-    initialLoadedRef.current = true;
-
-    const image = new Image();
-    image.src = 'src/assets/filter/sunglasses.png';
-
-    setStatus('Load Model...');
-
-    loadFaceLandmarker().then((model) => {
-      setStatus('Model Loaded');
-      requestAnimationFrame(() =>
-        estimateFacesLoop(model, image, canvasContext),
-      );
-    });
-  }, [estimateFacesLoop]);
+  useEffect(() => {
+    if (!image || !canvasContext || !faceLandmarker) return;
+    requestAnimationFrame(() =>
+      estimateFacesLoop(faceLandmarker, image, canvasContext),
+    );
+  }, [estimateFacesLoop, faceLandmarker, image, canvasContext]);
 
   return (
     <>
