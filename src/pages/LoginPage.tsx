60798e107bbddb598835cf2c30624b0c528697ba
Parksangwoo <psu8430@gmail.com>
2024-07-09 22:21:55 +0900

    fix: prevent user enter login/signup page when user has auth info

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,12 @@
+import { Navigate } from 'react-router-dom';
 import LoginForm from '../components/login/LoginForm';
+import routes from '../constants/routes';
+import { getCookie } from '../utils/cookie';
 
 const LoginPage = () => {
+  const token = getCookie('authorization');
+
+  if (token) return <Navigate to={routes.main} replace />;
   return (
     <div className="flex h-full items-center">
       <LoginForm />
--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,6 +1,13 @@
+import { Navigate } from 'react-router-dom';
 import SignupForm from '../components/signup/SignupForm';
+import routes from '../constants/routes';
+import { getCookie } from '../utils/cookie';
 
 const SignupPage = () => {
+  const token = getCookie('authorization');
+
+  if (token) return <Navigate to={routes.main} replace />;
+
   return <SignupForm />;
 };
 
