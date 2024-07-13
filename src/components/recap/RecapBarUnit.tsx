ea1cbcb36736e522d552bdb385b2784b77711240
Parksangwoo <psu8430@gmail.com>
2024-07-14 03:43:50 +0900

    design: Modify Css of recap header bar

--- src/components/recap/RecapBarUnit.tsx
+++ src/components/recap/RecapBarUnit.tsx
@@ -1,9 +1,19 @@
-import clsx from "clsx"
+import clsx from 'clsx';
 
-const RecapBarUnit = ({isBright}: {isBright: boolean}) => {
-  const unitClass = clsx('h-1 w-full', isBright ? 'bg-gray-40' : "bg-gray-20");
+const RecapBarUnit = ({
+  isBright,
+  isTarget,
+}: {
+  isBright: boolean;
+  isTarget: boolean;
+}) => {
+  const unitClass = clsx(
+    'linear h-1 transition',
+    isTarget ? 'w-full' : 'w-[100px]',
+    isBright ? 'bg-gray-40' : 'bg-gray-20',
+  );
 
-  return <div className={unitClass}></div>
-}
+  return <div className={unitClass}></div>;
+};
 
 export default RecapBarUnit;
