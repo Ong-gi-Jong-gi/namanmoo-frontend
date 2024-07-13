5e8e96a4259ff6db30226277b856b03f81df41ef
Parksangwoo <psu8430@gmail.com>
2024-07-14 03:44:30 +0900

    feat: Add Recap Appreciation page

---
+++ src/components/recap/RecapAppreciations.tsx
@@ -0,0 +1,27 @@
+import { useGetRecapMigos } from '../../apis/recap/getRecapAppreciations.ts';
+import { RecapMigos } from '../../types/recap.ts';
+import RecapThanksAndSorryUnit from './RecapAppreciationsUnit.tsx';
+
+const RecapAppreciations = ({ luckyId }: { luckyId: string }) => {
+  const { migos, isLoading } = useGetRecapMigos({ luckyId });
+
+  if (isLoading) <div>Loading...</div>;
+
+  return (
+    <div className="relative h-full">
+      <p className="w-full whitespace-pre-line py-4 text-right text-pretendard-md font-pretendard-bold">
+        가족들과
+        <br />
+        무언가를 함께한다는 건<br />
+        정말 행복한 일이죠.
+      </p>
+      <div className="h-full w-full">
+        {migos.map((migo: RecapMigos, index: number) => (
+          <RecapThanksAndSorryUnit key={migo.userId} {...migo} index={index} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RecapAppreciations;
