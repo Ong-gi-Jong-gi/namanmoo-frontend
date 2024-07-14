88f45fd18ac25635ef6764880180e5680fcf2661
bada308 <203190@jnu.ac.kr>
2024-07-14 14:59:21 +0900

    feat: 화상채팅 챌린지 상태 확인 컴포넌트 구현

---
+++ src/components/face-challenge/facetime/StatusBar.tsx
@@ -0,0 +1,44 @@
+import clsx from 'clsx';
+import useSocket from '../../../hooks/useSocket';
+import { useFacetimeChallengeStore } from '../../../store/facetimeChallengeStore';
+
+interface StatusBarProps {
+  code: string;
+}
+
+const StatusBar = ({ code }: StatusBarProps) => {
+  const { status, remainingTime } = useFacetimeChallengeStore();
+  const { emitChallengeStart } = useSocket();
+  const handleChallengeStart = () => {
+    emitChallengeStart(code);
+  };
+  const renderTime = remainingTime % 10 || 10;
+
+  const timerClass = clsx('w-full text-center font-ryurue text-ryurue-md', {
+    'text-white': renderTime > 5,
+    'text-red': renderTime <= 5,
+  });
+
+  return (
+    <div className="bg-black">
+      {status === 'idle' && (
+        <button
+          className="w-full text-center font-ryurue text-ryurue-md text-white"
+          onClick={handleChallengeStart}
+        >
+          챌린지 시작
+        </button>
+      )}
+      {status === 'ongoing' && (
+        <p className={timerClass}>{remainingTime % 10 || 10}</p>
+      )}
+      {status === 'finished' && (
+        <p className="w-full text-center font-ryurue text-ryurue-md text-white">
+          챌린지 종료
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default StatusBar;
