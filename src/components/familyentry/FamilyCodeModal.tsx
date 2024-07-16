2e986a445ea308bbe6a10b2b905892e4b46d8233
Parksangwoo <psu8430@gmail.com>
2024-07-17 01:27:55 +0900

    fix: 모든 구성원이 참여한 가족에 참여할 수 있는 현상 수정

--- src/components/familyentry/FamilyCodeModal.tsx
+++ src/components/familyentry/FamilyCodeModal.tsx
@@ -16,9 +16,6 @@ const FamilyCodeModal = ({ code }: { code: string }) => {
 
   const handleJoinBtn = () => {
     setEnabled(true);
-    if (hasData) {
-      moveNextModal();
-    }
   };
 
   const moveNextModal = () => {
