79dec836e80706139db52661b27ce9bf5a6ad499
Parksangwoo <psu8430@gmail.com>
2024-07-17 20:02:55 +0900

    feat: 통계 리캡에서 보여주는 지표들을 타입에 따라 구별

--- src/components/recap/RecapStatisticsTopic.tsx
+++ src/components/recap/RecapStatisticsTopic.tsx
@@ -1,6 +1,8 @@
 import { useNavigate } from 'react-router-dom';
+import { RECAP_STATISTICS_QUESTION } from '../../constants/recap';
 import routes from '../../constants/routes';
 import { RecapStatistics } from '../../types/recap';
+import { formatTime } from '../../utils/formatter';
 
 const RecapStatisticsTopic = ({
   topic,
@@ -16,19 +18,36 @@ const RecapStatisticsTopic = ({
     });
   };
 
+  const handleStatistics = {
+    mostViewed: (value: number) => {
+      return `${new Intl.NumberFormat('en-US').format(value)} 회`;
+    },
+    fastestAnswered: (value: number) => {
+      return `${formatTime(value)}`;
+    },
+  } as const;
+
   return (
     <div className="relative z-20" onClick={handleTopiClick}>
       <h3 className="relative z-30 rounded-full bg-secondary-10 px-4 py-1 font-ryurue text-ryurue-base">
         <span className="mr-3 text-secondary-20">Q.</span>
-        {topic}?
+        {
+          RECAP_STATISTICS_QUESTION[
+            topic as keyof typeof RECAP_STATISTICS_QUESTION
+          ]
+        }
       </h3>
       <div className="relative -top-4 z-0 m-auto w-[95%] rounded-bl-xl rounded-br-xl bg-gray-20 p-4 pt-8">
         <div className="flex flex-col gap-2 rounded-lg bg-white p-3 font-ryurue text-ryurue-base">
           <p className="text-pretendard-md text-secondary-20">{`#${parseInt(challengeNumber) > 10 ? challengeNumber : '0' + challengeNumber}`}</p>
           <p className="h-full truncate text-ellipsis text-ryurue-base">
-            {`${challengeTitle}`}
+            {challengeTitle}
           </p>
-          <p className="w-full text-right">{topicResult} 회</p>
+          <p className="w-full text-right">
+            {handleStatistics[topic as keyof typeof RECAP_STATISTICS_QUESTION](
+              topicResult,
+            )}
+          </p>
         </div>
       </div>
     </div>
--- src/constants/recap.ts
+++ src/constants/recap.ts
@@ -1,1 +1,6 @@
-export const RECAP_LENGTH = 7
+export const RECAP_LENGTH = 7;
+
+export const RECAP_STATISTICS_QUESTION = {
+  mostViewed: '가장 많이 조회된 질문은?',
+  fastestAnswered: '가장 빨리 응답이 완성된 질문은?',
+} as const;
