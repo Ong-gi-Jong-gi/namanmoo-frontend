430171dc0e44db17669b11c486a28db692b2c08c
bada308 <203190@jnu.ac.kr>
2024-07-19 14:43:42 +0900

    feat: 메인 페이지 fetch 변경

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,11 +1,12 @@
 import QueryString from 'qs';
-import { useEffect } from 'react';
+import { Suspense, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useGetChallengeStartDate } from '../apis/challenge/getChallengeStartDate';
 import { useGetMyFamilyInfo } from '../apis/family/getMyFamilyInfo';
+import Loader from '../components/common/Loader';
 import ChallengeSection from '../components/main/ChallengeSection';
 import FamilyList from '../components/main/FamilyList';
-import InviteModal from '../components/main/InviteModal';
+// import InviteModal from '../components/main/InviteModal';
 import Navbar from '../components/main/Navbar';
 import routes from '../constants/routes';
 import useModalStore from '../store/modalStore';
@@ -21,30 +22,26 @@ const MainPage = () => {
   });
   const { openModal } = useModalStore();
 
-  // useEffect(() => {
-
-  //   const challengeDate = localStorage.getItem('challengeStartDate');
-  //   localStorage.setItem('challengeStartData', challengeDate);
-  // }, [challengeDate]);
-
   useEffect(() => {
-    if (!familyLoading && familyList?.length === 1) {
-      openModal({
-        content: <InviteModal code={queryData['code'] as string} />,
-        showCloseBtn: true,
-      });
-    }
+    // if (!familyLoading && familyList?.length === 1) {
+    //   openModal({
+    //     content: <InviteModal code={queryData['code'] as string} />,
+    //     showCloseBtn: true,
+    //   });
+    // }
   }, [familyList, familyLoading, openModal, queryData]);
 
   if (familyLoading && startDateLoading) return <div>가족 정보 Loading...</div>;
   if (!familyList) return <Navigate to={routes.family.entry} />;
 
   return (
-    <div className="h-full w-full">
-      <FamilyList familyList={familyList} />
-      <Navbar />
-      <ChallengeSection currentFamilySize={familyList.length} />
-    </div>
+    <Suspense fallback={<Loader />}>
+      <div className="h-full w-full">
+        <FamilyList familyList={familyList} />
+        <Navbar />
+        <ChallengeSection currentFamilySize={familyList.length} />
+      </div>
+    </Suspense>
   );
 };
 
