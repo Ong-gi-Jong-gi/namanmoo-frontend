7986289a539f4c58843ee4a19fc33a2eac7d0010
Parksangwoo <psu8430@gmail.com>
2024-07-23 13:59:41 +0900

    chore: bottomsheet z-index 수정

--- src/components/common/BottomSheet.tsx
+++ src/components/common/BottomSheet.tsx
@@ -21,13 +21,13 @@ const BottomSheetProvider = () => {
     'fixed bottom-0 left-1/2 mx-auto h-fit w-full max-w-[450px] -translate-x-1/2 transform rounded-t-3xl bg-secondary-10 p-5 shadow-shadow-box transition-transform duration-300',
     isOpen
       ? 'translate-y-0 animate-slide-up'
-      : 'animate-slide-down translate-y-full',
+      : 'translate-y-full animate-slide-down',
   );
 
   return (
     <div
       className={clsx(
-        'fixed inset-0 bg-black bg-opacity-30 transition-opacity duration-300',
+        'fixed inset-0 z-50 bg-black bg-opacity-30 transition-opacity duration-300',
         { 'opacity-100': isOpen, 'pointer-events-none opacity-0': !isOpen },
       )}
     >
