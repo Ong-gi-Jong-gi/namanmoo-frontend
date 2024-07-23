83ace3e36636cd724f069f93111fc762990c5fc0
Parksangwoo <psu8430@gmail.com>
2024-07-23 14:03:29 +0900

    chore: 바텀 시트 스타일을 모달 컴포넌트와 통일

--- src/components/common/BottomSheet.tsx
+++ src/components/common/BottomSheet.tsx
@@ -27,7 +27,7 @@ const BottomSheetProvider = () => {
   return (
     <div
       className={clsx(
-        'fixed inset-0 z-50 bg-black bg-opacity-30 transition-opacity duration-300',
+        'fixed inset-0 z-50 bg-black bg-opacity-30 backdrop-blur-sm transition-opacity duration-300',
         { 'opacity-100': isOpen, 'pointer-events-none opacity-0': !isOpen },
       )}
     >
