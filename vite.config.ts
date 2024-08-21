e1ed6defdd785df48a4c90f469a36770c0b6b6e8
Parksangwoo <psu8430@gmail.com>
2024-08-21 16:39:02 +0900

    setting: 로컬 환경에서 https로 접속하기 위한 세팅 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
@@ -7,6 +9,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
+    https: {
+      key: fs.readFileSync(path.resolve('./cert', 'localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve('./cert', 'localhost.pem')),
+    },
   },
   define: {
     global: {},
