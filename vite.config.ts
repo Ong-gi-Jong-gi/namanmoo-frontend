5a68511a5bd9f584eaf7e16d76fcc26b99ebad84
Parksangwoo <psu8430@gmail.com>
2024-08-25 20:15:52 +0900

    chore: 환경변수 사용 용법 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,23 +4,25 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    https:
-      process.env.VITE_MODE === 'production'
-        ? {}
-        : {
-            key: fs.readFileSync(path.resolve('./cert', 'localhost-key.pem')),
-            cert: fs.readFileSync(path.resolve('./cert', 'localhost.pem')),
-          },
-  },
-  define: {
-    global: {},
-  },
-  build: {
-    chunkSizeWarningLimit: 1600,
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      https:
+        mode === 'production'
+          ? {}
+          : {
+              key: fs.readFileSync(path.resolve('./cert', 'localhost-key.pem')),
+              cert: fs.readFileSync(path.resolve('./cert', 'localhost.pem')),
+            },
+    },
+    define: {
+      global: {},
+    },
+    build: {
+      chunkSizeWarningLimit: 1600,
+    },
+  };
 });
